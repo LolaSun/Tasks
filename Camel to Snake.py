5f152b8f2a05eb78a7a73d18273d1cc4ee0b8ae3
Lola <74794836+LolaSun@users.noreply.github.com>
2020-11-23 18:08:33 +0200

    Create Camel to Snake.py

---
+++ Camel to Snake.py
@@ -0,0 +1,25 @@
+"""
+The company you are working for is refactoring its entire codebase. It's changing all naming conventions from camel to snake case (camelCasing to snake_casing).  
+Every capital letter is replaced with its lowercase prefixed by an underscore _, except for the first letter, which is lowercased without the underscore, so that SomeName becomes some_name. 
+ 
+Task:  
+Write a program that takes in a string that has camel casing, and outputs the same string but with snake casing. 
+ 
+Input Format:  
+A string with camelCasing. 
+ 
+Output Format:  
+The same string but with snake_casing. 
+ 
+Sample Input:  
+camelCasing 
+ 
+Sample Output: 
+camel_casing
+"""
+
+import re
+camel=input()
+pattern=r".[a-z]*"
+snake="_".join(re.findall(pattern, camel)).lower()
+print(snake)
