4b4ed44ec566a54cd6aed43b32776d0c467c2b52
Lola <74794836+LolaSun@users.noreply.github.com>
2020-11-23 18:17:13 +0200

    Update Snowballing Numbers.py

--- Snowballing Numbers.py
+++ Snowballing Numbers.py
@@ -3,7 +3,8 @@ You are given a list of numbers in a particular order. You need to check to see 
 If they are, you have created successful snowball numbers. 
  
 Task:  
-Create a program that takes in an array of numbers, check if each number is greater than the sum of all previous numbers, and output true if the condition is met, and false, if not. 
+Create a program that takes in an array of numbers, check if each number is greater than the sum of all previous numbers, and output true if the condition is met,
+and false, if not. 
  
 Input Format:  
 The first input denotes the length of the list (N). The next N lines contain the list elements as integers.  
