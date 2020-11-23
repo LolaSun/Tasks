baa2a2616fecc2290f8326e072b4229c1f925c3a
Lola <74794836+LolaSun@users.noreply.github.com>
2020-11-23 18:32:42 +0200

    Update Initials.py

--- Initials.py
+++ Initials.py
@@ -1,8 +1,10 @@
 """
-You are given a list of names for a fundraiser, but you need to keep the names relatively anonymous. You are tasked with getting the initials for each name provided. 
+You are given a list of names for a fundraiser, but you need to keep the names relatively anonymous. You are tasked
+with getting the initials for each name provided. 
  
 Task:  
-Given a list of first and last names, take the first letter from each name to create initials and output them as a space-separated string. 
+Given a list of first and last names, take the first letter from each name to create initials and output them as a 
+space-separated string. 
  
 Input Format:  
 The first input denotes the number of names in the list (N). The next N lines contain the list elements as strings.  
