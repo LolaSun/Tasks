a97355ba0227c97d655e91dec798183aea21e6a8
Lola <74794836+LolaSun@users.noreply.github.com>
2020-11-23 18:09:08 +0200

    Update It's a Sign.py

--- It's a Sign.py
+++ It's a Sign.py
@@ -1,4 +1,5 @@
-"""You work in a sign factory, and the machine has started printing all of the signs backwards! The signs have already been packed into boxes of four signs each. 
+"""
+You work in a sign factory, and the machine has started printing all of the signs backwards! The signs have already been packed into boxes of four signs each. 
 Every letter on every sign is capitalized, so if the original sign was of a palindrome (a word or phrase that is the same backwards and forwards) 
 you can still save those signs and not have to reprint them. Check each box to see if you should open it up to dig out the sign you can save, 
 or if you need to just throw the whole box in the trash. 
@@ -19,7 +20,8 @@ RACECAR
 TACOS 
  
 Sample Output  
-Open"""
+Open
+"""
 
 words=[input() for i in range(4)]
 for i in words:
