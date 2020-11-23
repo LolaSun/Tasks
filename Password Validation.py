a3be367443c81b843f93f2e1b27b4bcdadde9a40
Lola <74794836+LolaSun@users.noreply.github.com>
2020-11-23 18:27:59 +0200

    Update Password Validation.py

--- Password Validation.py
+++ Password Validation.py
@@ -1,8 +1,11 @@
 """
-You're interviewing to join a security team. They want to see you build a password evaluator for your technical interview to validate the input. 
+You're interviewing to join a security team. They want to see you build a password evaluator for your technical 
+interview to validate the input. 
  
 Task:  
-Write a program that takes in a string as input and evaluates it as a valid password. The password is valid if it has at a minimum 2 numbers, 2 of the following special characters ('!', '@', '#', '$', '%', '&', '*'), and a length of at least 7 characters. 
+Write a program that takes in a string as input and evaluates it as a valid password. The password is 
+valid if it has at a minimum 2 numbers, 2 of the following special characters ('!', '@', '#', '$', '%', '&', '*'),
+and a length of at least 7 characters. 
 If the password passes the check, output 'Strong', else output 'Weak'. 
  
 Input Format: 
